04a125cb729c731ef405d1c03a42cec9345cbfe2
charmedbns <charmedbns@gmail.com>
2020-04-05 18:24:33 +0300

    Initial Commit

---
+++ swiftbook UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftbook UI
+//
+//  Created by Alexey on 05/04/2020.
+//  Copyright © 2020 Alexey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
